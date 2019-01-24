069a1d5cc93e02b9dd79aaf45c7347f370efea93
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-24 09:50:25 +0100

    inserito messaggio

---
+++ cambioConsolle/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cambioConsolle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ciao");
+        }
+    }
+}
